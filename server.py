24996f4c28075132f5c90c81563dab54eaffc493
DiegoAsmat12 <diegoas1205@gmail.com>
2022-01-21 15:51:04 -0500

    Agregando el numero de veces que se visita la pagina aparte del contador

--- server.py
+++ server.py
@@ -7,9 +7,11 @@ app.secret_key= "secret_key"
 def vistaContador():
     if('contador' in session):
         session["contador"]+=session["incremento"]
+        session["contador_real"]+=1
     else:
         session["contador"]=1
         session["incremento"]=1
+        session["contador_real"]=1
     return render_template('index.html')
 
 @app.route("/", methods=["POST"])
@@ -19,6 +21,7 @@ def aumentaContador():
         if 'incremento' in session:
             session["incremento"]= int(request.form["incremento"])
             session["contador"]+=session["incremento"]
+            session["contador_real"]+=1
         else:
             print("el valor de incremento no existe!")
             return redirect("/")
@@ -32,6 +35,7 @@ def aumentaContador2():
     if 'contador' in session:
         print('el contador existe!')
         session["contador"]+=2
+        session["contador_real"]+=1
         return redirect("/count")
     else:
         print('el contador NO existe!')
